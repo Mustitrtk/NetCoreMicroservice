2d33241e6efac7676c8b828265d8484cb0709078
Mustafa TORTUK <mustafat237@hotmail.com>
2025-08-02 01:37:19 +0300

    Entities added.

---
+++ NetCoreMicroservice.Catalog.API/Features/Categories/Category.cs
@@ -0,0 +1,11 @@
+﻿using NetCoreMicroservice.Catalog.API.Features.Courses;
+using NetCoreMicroservice.Catalog.API.Repository;
+
+namespace NetCoreMicroservice.Catalog.API.Features.Categories
+{
+    public class Category : BaseEntity
+    {
+        public String Name { get; set; } = default!;
+        public List<Course>? Courses { get; set; }
+    }
+}
---
+++ NetCoreMicroservice.Catalog.API/Repository/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace NetCoreMicroservice.Catalog.API.Repository
+{
+    public class BaseEntity
+    {
+        [BsonElement("_id")] 
+        public Guid Id { get; set; }
+    }
+}
