34ed1a4a100152642a53d38fd838111ecc56980a
Mustafa TORTUK <mustafat237@hotmail.com>
2025-08-02 15:12:25 +0300

    AppDbContext added.

---
+++ NetCoreMicroservice.Catalog.API/Repository/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreMicroservice.Catalog.API.Features.Categories;
+using NetCoreMicroservice.Catalog.API.Features.Courses;
+using System.Reflection;
+
+namespace NetCoreMicroservice.Catalog.API.Repository
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> option) : DbContext(option)
+    {
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
---
+++ NetCoreMicroservice.Catalog.API/Repository/CategoryEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MongoDB.EntityFrameworkCore.Extensions;
+using NetCoreMicroservice.Catalog.API.Features.Categories;
+
+namespace NetCoreMicroservice.Catalog.API.Repository
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToCollection("categories");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Name).HasMaxLength(100);
+            builder.Ignore(x => x.Courses);
+        }
+    }
+}
