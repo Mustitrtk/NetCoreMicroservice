aa86bfb97027316a7bc3d5379d842c8c0f932394
Mustafa TORTUK <mustafat237@hotmail.com>
2025-10-11 21:28:36 +0300

    FluentValidation added & Generic validation filter created.

--- NetCoreMicroservice.Catalog.API/Features/Categories/Create/CreateCategoryEndpoint.cs
+++ NetCoreMicroservice.Catalog.API/Features/Categories/Create/CreateCategoryEndpoint.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using NetCoreMicroservice.Shared.Extensions;
+using NetCoreMicroservice.Shared.Filters;
 
 namespace NetCoreMicroservice.Catalog.API.Features.Categories.Create
 {
@@ -10,7 +11,7 @@ namespace NetCoreMicroservice.Catalog.API.Features.Categories.Create
         public static RouteGroupBuilder CreateCategoryGroupItemEndpoint(this RouteGroupBuilder group)
         {
             group.MapPost("/", 
-                async (CreateCategoryCommand command, IMediator mediator) => (await mediator.Send(command)).ToResult());
+                async (CreateCategoryCommand command, IMediator mediator) => (await mediator.Send(command)).ToResult()).AddEndpointFilter<ValidationFilters<CreateCategoryCommand>>();
 
             return group;
         }
